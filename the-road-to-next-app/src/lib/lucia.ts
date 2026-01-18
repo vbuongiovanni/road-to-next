3545914c1968d9be1e7485498580b2dc40808ddb
Vince b <vince.b@doorcast.com>
2026-01-17 21:54:43 -0700

    adding prior changes to raod the next app

---
+++ the-road-to-next-app/src/lib/lucia.ts
@@ -0,0 +1,27 @@
+import { PrismaAdapter } from '@lucia-auth/adapter-prisma';
+import { Lucia } from 'lucia';
+import { prisma } from './prisma';
+
+const adapter = new PrismaAdapter(prisma.session, prisma.user);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {},
+  getUserAttributes: (attributes) => {
+    return {
+      username: attributes.username,
+      email: attributes.email,
+    };
+  },
+});
+
+declare module 'lucia' {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+interface DatabaseUserAttributes {
+  username: string;
+  email: string;
+}
